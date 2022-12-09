1cd4dd84f7b727a9b2d5e2a92bf2cdd4e4384a42
maksimcialehin <madqfrog1863@gmail.com>
2022-12-09 20:15:20 +0300

    styling images

--- src/screens/components/detail_view.js
+++ src/screens/components/detail_view.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, Image } from "react-native"
+import { View, Text, Image } from "react-native"
+import { FlatList } from "react-native-gesture-handler";
 import client from "../../api/client";
+import styles from "./detail_style";
 
 const DetailView = ({ navigation, route }) => {
     const [detail, setDetail] = useState("");
@@ -22,38 +24,28 @@ const DetailView = ({ navigation, route }) => {
     useEffect(() => { getDetail(objurl); }, []);
     return (
         <View style={styles.center}>
-            <Image
-                style={styles.pizzaImage}
-                source={{
-                    uri: detail.logo_image,
+            <FlatList
+                horizontal={true}
+                data={detail.pizzeria_images}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem = {({item}) => {
+                    return (
+                        <Image
+                            style={styles.pizzaImage}
+                            source={{ uri: item.image, }}
+                        />
+                    )
                 }}
             />
             <Text style={styles.title}>Restaurant: {detail.restaurant_name}</Text>
-            <Text style={styles.title}>Address: {detail.street}</Text>
-            <Text style={styles.title}>City: {detail.city}, {detail.zip_code}</Text>
-            <Text style={styles.title}>Web: {detail.website}</Text>
-            <Text style={styles.title}>Phone: {detail.phone_number}</Text>
-            <Text style={styles.title}>Description: {detail.description}</Text>
-            <Text style={styles.title}>Email: {detail.email}</Text>
+            <Text style={styles.details}>Address: {detail.street}</Text>
+            <Text style={styles.details}>City: {detail.city}</Text>
+            <Text style={styles.details}>Web: {detail.website}</Text>
+            <Text style={styles.details}>Phone: {detail.phone_number}</Text>
+            <Text style={styles.details}>Description: {detail.description}</Text>
+            <Text style={styles.details}>Email: {detail.email}</Text>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    title: {
-        fontSize: 15,
-        marginBottom: 16,
-    },
-    pizzaImage: {
-        width: 200,
-        height: 200,
-        marginBottom: 16,
-    },
-});
 
 export default DetailView;
